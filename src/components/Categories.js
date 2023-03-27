9c63946727a592987ee9145cd423a14f6f63142c
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-03-27 21:16:36 +0300

    Set up Categories page to collect and display category data from the backend.

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,11 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import './Categories.css';
 import EachCategory from './EachCategory';
 
 function Categories() {
+    const [categories, setCategories] = useState([])
+
+    // fetching categories from the backend
+    useEffect(() => {
+        fetch("/categories")
+        .then((r) => r.json())
+        .then(setCategories)
+    }, [])
+
+
+    let onecategory = categories.map((category) => {
+        return (
+            <EachCategory key={category.id} category={category}/>
+        )
+    })
     return (
         <Fragment>
-            <p>Categories</p>
+            <div className='cards'>
+                <h2>Pick a Category to Order From</h2>
+                <div className='cards__container'>
+                    <div className='cards__wrapper'>
+                        <ul className='cards__items'>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </Fragment>
     )
 }
